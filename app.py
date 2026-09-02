a0ac997f3749a5c518037b269494b2f247014352
gianlucascardaci-create <gianlucascardaci@gmail.com>
2026-09-02 19:59:34 +0200

    app.py

--- app.py
+++ app.py
@@ -1729,7 +1729,7 @@ def salva_dati_esterni():
         else:
           supabase.table("eventi_catering").insert(ev_to_save).execute()
 
-    for usr_k, usr_v in st.file_uploader.session_state.utenti_autorizzati.items():
+    for usr_k, usr_v in str_lit.file_uploader.session_state.utenti_autorizzati.items():
       u_data = {
           "username": usr_k,
           "password": usr_v.get("password"),
@@ -1743,7 +1743,7 @@ def salva_dati_esterni():
       else:
         supabase.table("utenti_autorizzati").insert(u_data).execute()
 # Pulsante per caricare l'immagine del singolo prodotto
-file_caricato = st.file_uploader(
+file_caricato =str_lit.file_uploader(
     "Carica foto", type=["jpg", "png", "jpeg"], key="upload_prodotto_nuovo"
 )
 
